98ee177494de92e2e64895f7f9b3db6a98e21678
Chitrarath Kumar <chitrarath.kumar@gmail.com>
2018-11-13 14:28:22 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,1 +1,6 @@
+node{
 checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'bd99e736-db20-4244-aec5-e0c6c8f6b7fd', url: 'https://github.com/chitrarathkumar/angularnode.git']]])
+stage 'build'
+    sh "docker build -t maven-jdk-8-docker-image:$BUILD_ID ."
+    sh "docker tag maven-jdk-8-docker-image:$BUILD_ID maven-jdk-8-docker-image:latest"
+}
